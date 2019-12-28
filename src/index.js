a81bd7f85858c504757ef4ae9eda4282b6c606c1
Mauricio Contreras <virtual.kur2@gmail.com>
2019-12-28 22:41:11 +0100

    require db.helper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-
+const server = require('./server');
+const dbHelper = require('./server/helpers/db.helper');
 // Console Stamp basic
 require('console-stamp')(console);
-console.log('Starting...');
+
+console.log('Starting app...');
+const app = server();
+dbHelper.connect()
+  .then(() => {
+    console.info('Starting server...');
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}.`);
+  });
+  
