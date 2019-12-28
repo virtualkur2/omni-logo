18681e48b7579cbbd22d3e22a14ef049f760fc6c
Mauricio Contreras <virtual.kur2@gmail.com>
2019-12-28 22:48:15 +0100

    server.listen method added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const server = require('./server');
 const dbHelper = require('./server/helpers/db.helper');
+const PORT = require('./config').env.PORT;
+
 // Console Stamp basic
 require('console-stamp')(console);
 
@@ -8,8 +10,15 @@ const app = server();
 dbHelper.connect()
   .then(() => {
     console.info('Starting server...');
+    app.listen(PORT, (err) => {
+      if(err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
+      }
+      console.info(`Server started and listening on port: ${PORT}.`);
+    });
   })
   .catch((error) => {
     console.error(`Error: ${error.message}.`);
+    process.exit(1);
   });
-  
