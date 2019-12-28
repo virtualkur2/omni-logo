c32fc16b2f8900aaf71cdba532ebc3a99ffc3d3a
Mauricio Contreras <virtual.kur2@gmail.com>
2019-12-28 15:31:20 +0100

    server file modified to include cokkie parser and routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const config = require('../config');
+const routes = require('./routes');
 
 const router = express.Router();
 const server = () => {
@@ -7,6 +11,8 @@ const server = () => {
   app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true}));
+  app.use(cookieParser(config.cookie.SECRET));
+  app.use(routes(router));
   return app;
 }
 
