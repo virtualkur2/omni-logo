1cd11554ef64d9456c54e6c0cd18e1af2055e812
Mauricio Contreras <virtual.kur2@gmail.com>
2020-01-01 17:34:15 +0100

    user and auth routes updated

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,9 @@
-
+const authRoutes = require('./auth');
+const usersRoutes = require('./users');
 
 const routes = (router) => {
+  authRoutes(router);
+  usersRoutes(router);
   return router;
 }
 
