812b61e7fb37d6f6d2376718ceaf324a31e36221
Mauricio Contreras <virtual.kur2@gmail.com>
2020-01-19 11:06:44 +0100

    console info to show public path of static files

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const config = require('../config');
 const routes = require('./routes');
 const notFoundHelper = require('./helpers/notfound.helper');
 const errorHelper = require('./helpers/error.helper');
+
+const staticOptions = config.serveStatic.options;
+
 
 const router = express.Router();
 const server = () => {
@@ -15,6 +19,7 @@ const server = () => {
   app.use(morgan('combined'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true}));
+  app.use(express.static(path.join(__dirname, config.paths.public)));
   app.use(cookieParser(config.cookie.SECRET));
   app.use(routes(router));
   app.use(notFoundHelper);
