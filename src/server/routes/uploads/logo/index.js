a5761e0fa04fb04314e682c407a72997b2b9d78b
Mauricio Contreras <virtual.kur2@gmail.com>
2020-01-26 12:13:41 +0100

    logo route updated

--- src/server/routes/uploads/logo/index.js
+++ src/server/routes/uploads/logo/index.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const authController = require('../../../controllers/auth.controller');
+const logoController = require('../../../controllers/logo.controller');
 const baseAPIpath = require('../../../../config').env.BASE_URI;
 
 const logoRoutes = (router) => {
-  router.route(path.join(baseAPIpath, 'logo'))
-    .get(authController.requireSignin);
+  router.route(path.join(baseAPIpath, 'logos'))
+    // .get(authController.requireSignin);
+    .get(authController.requireSignin, logoController.list)
+    .post(authController.requireSignin, logoController.uploadArray, logoController.create)
+    .delete(authController.requireSignin, authController.hasAuthorization, logoController.remove);
+
+  router.route(path.join(baseAPIpath, 'logos/id/:logoId'))
+    .get(authController.requireSignin, logoController.list)
+    .delete(authController.requireSignin, authController.hasAuthorization, logoController.remove);
+  
+  router.param('logoId', logoController.logoById);
   
   return router;
 }
